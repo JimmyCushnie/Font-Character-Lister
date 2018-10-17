8ed1aded2ca2f494b4e04a04cd295dfc26918eac
Jimmy Cushnie <jimmycushnie@gmail.com>
2018-10-17 01:38:59 -0400

    gragfrs

---
+++ Font Character List/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Media;
+using System.Windows;
+
+namespace Font_Character_List
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            start:
+
+            Console.WriteLine("enter path of font");
+            string path = Console.ReadLine();
+
+            GlyphTypeface font;
+            try
+            {
+                font = new GlyphTypeface(new Uri(path));
+            }
+            catch
+            {
+                Console.WriteLine("there's no font there you IDIOT");
+                goto start;
+            }
+
+            var charactersList = font.CharacterToGlyphMap.Keys;
+            string charactersString = "";
+            foreach(var characterInt in charactersList)
+            {
+                if (characterInt == 0) continue;
+                charactersString += (char)characterInt;
+            }
+
+            Clipboard.SetText(charactersString, TextDataFormat.UnicodeText);
+            Console.WriteLine("All font characters copied to cliboard");
+
+            goto start;
+        }
+    }
+}
